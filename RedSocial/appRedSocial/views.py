6bcc8d39c44dce95c9cf232a736085b79e675afd
Zabaleta99 <jon.zabaleta@opendeusto.es>
2020-11-15 18:58:53 +0100

    Landing apge

--- RedSocial/appRedSocial/views.py
+++ RedSocial/appRedSocial/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 def index(request):
-    return HttpResponse("Listado de departamentos")
+    return render(request, 'index.html')
