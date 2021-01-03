b0e2ade3a4a1cd61634b4eb1d0de1d1e210338f2
Zabaleta99 <jon.zabaleta@opendeusto.es>
2021-01-03 14:15:28 +0100

    jquery qtip plugin

---
+++ RedSocial/appRedSocial/static/js/script1.js
@@ -0,0 +1,22 @@
+$.fn.qtip.styles.mystyle = { // Definir estilo que aplicaremos después
+   width: 200,
+   background: '#A2D959',
+   color: 'black',
+   textAlign: 'center',
+   border: {
+      width: 0.5,
+      radius: 2,
+      color: '#A2D959'
+   },
+   tip: 'bottomLeft',
+   name: 'dark' 
+}
+
+$(document).ready(function() {
+  //jQuery code...
+  $('#paisvasco').qtip(
+  {
+    content: 'Una comunidad autónoma es una especie de región, un estado suele tener varios.',
+    style: 'mystyle'
+  });
+});
